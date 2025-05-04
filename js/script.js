d88c164fb8e390cca0bb10b4135a874556747bf7
Jorge Renato <jorgerenato032@gmail.com>
2025-05-04 17:54:16 -0400

    main

---
+++ js/script.js
@@ -0,0 +1,24 @@
+document.getElementById('voltar-topo').addEventListener('click', function(e) {
+    e.preventDefault(); // impede o pulo seco do href="#"
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+document.getElementById('voltar-about').addEventListener('click', function(e) {
+    e.preventDefault(); // impede o pulo seco do href="#"
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+document.getElementById('voltar-project').addEventListener('click', function(e) {
+    e.preventDefault(); // impede o pulo seco do href="#"
+    window.scrollTo({ top: 700, behavior: 'smooth' });
+});
+
+document.getElementById('voltar-message').addEventListener('click', function(e) {
+    e.preventDefault(); // impede o pulo seco do href="#"
+    window.scrollTo({ top: 2000, behavior: 'smooth' });
+});
+
+document.getElementById('voltar-skills').addEventListener('click', function(e) {
+    e.preventDefault(); // impede o pulo seco do href="#"
+    window.scrollTo({ top: 1500, behavior: 'smooth' });
+});
